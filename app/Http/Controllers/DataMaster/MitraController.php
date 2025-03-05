dc00c19c60c90bf45262c47c3ea8f372f1311216
Dikha <nugrahaadi247@gmail.com>
2025-03-05 14:16:40 +0700

    fix bug mitra create

--- app/Http/Controllers/DataMaster/MitraController.php
+++ app/Http/Controllers/DataMaster/MitraController.php
@@ -7,6 +7,7 @@ use App\Models\Mitra;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class MitraController extends Controller
 {
